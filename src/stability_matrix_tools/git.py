a4a17b4fc3534d6d8d9ee6c52ddf12abea43f228
Ionite <dev@ionite.io>
2023-11-20 17:21:57 -0500

    Add custom repo merging commands

--- src/stability_matrix_tools/git.py
+++ src/stability_matrix_tools/git.py
@@ -1,20 +1,21 @@
 """Git routines and tools."""
-
-from tempfile import TemporaryDirectory
+import re
 
 import rich
 import typer
-
 from github import Auth, Github
-from github.GitRef import GitRef
-from github.Repository import Repository
 from github.Commit import Commit
+from github.Repository import Repository
+from typer import Option
+from typing_extensions import Annotated
 
 from stability_matrix_tools.models.keyring_config import ConfigKey, KeyringConfig
 from stability_matrix_tools.models.settings import env
 
 app = typer.Typer(no_args_is_help=True)
 cp = rich.print
+
+ConfirmType = Annotated[bool, Option("--yes", "-y", help="Confirm")]
 
 
 class GitContext:
@@ -41,6 +42,14 @@ class GitContext:
             env.git_repo_public.removeprefix("https://github.com/"), lazy=True
         )
 
+    def get_repo_from_url(self, url: str):
+        result = re.match(r"(?:https?://github.com/)?(.+?)/(.+?)(?:\.git)?$", url)
+        # Only get the first 2 groups
+        owner, name = result.groups()[:2]
+        return self.gh.get_repo(
+            f"{owner}/{name}", lazy=True
+        )
+
     @staticmethod
     def compare(base_repo: Repository, base: Commit, head_repo: Repository, head: Commit):
         head_part = f"{head_repo.owner.login}:{head_repo.name}:{head.sha}"
@@ -59,62 +68,55 @@ def auth():
     cp(f"Authenticated with GitHub as: {ctx.gh_user.login}")
 
 
-@app.command()
-def private_main_to_dev():
-    """Creates a PR to merge private/main into private/dev."""
+def pr_merge_branch(repo_url: str, from_branch: str, to_branch: str):
+    """Creates a PR to merge a repo's branch into another branch."""
     ctx = GitContext()
 
-    private_repo = ctx.get_private_repo()
-    private_repo_str = format_repo(private_repo)
-    private_main = private_repo.get_branch("main")
+    repo = ctx.get_repo_from_url(repo_url)
+    repo_str = format_repo(repo)
+
+    source = repo.get_branch(from_branch)
 
     # create a new branch from private/main
-    merge_branch_name = f"merge-main-to-dev-{private_main.commit.sha[:7]}"
-    private_repo.create_git_ref(
+    merge_branch_name = f"merge-{from_branch}-to-{to_branch}-{source.commit.sha[:7]}"
+    repo.create_git_ref(
         ref=f"refs/heads/{merge_branch_name}",
-        sha=private_main.commit.sha,
+        sha=source.commit.sha,
     )
 
-    cp(f"Creating branch: {private_repo_str}/{merge_branch_name} from {private_repo_str}/main @ {private_main.commit.sha[:7]}")
-    cp(f"Creating PR: {private_repo_str}/{merge_branch_name} -> {private_repo_str}/dev")
+    cp(f"Creating branch: {repo_str}/{merge_branch_name} from {repo_str}/{from_branch} @ {source.commit.sha[:7]}")
+    cp(f"Creating PR: {repo_str}/{merge_branch_name} -> {repo_str}/{to_branch}")
 
     # create a PR from private/main to private/dev
-    pr = private_repo.create_pull(
+    pr = repo.create_pull(
         title="Merge main to dev",
         body="",
-        base="dev",
+        base=to_branch,
         head=merge_branch_name,
     )
 
     cp(f"✅  Created PR: [cyan link={pr.url}]{pr.title} #{pr.number}[/cyan link]")
+
+
+@app.command()
+def main_to_dev(repo_url: str):
+    """Creates a PR to merge a repo's main branch into dev branch."""
+    pr_merge_branch(repo_url, "main", "dev")
+
+
+@app.command()
+def dev_to_main(repo_url: str):
+    """Creates a PR to merge a repo's main branch into dev branch."""
+    pr_merge_branch(repo_url, "dev", "main")
+
+
+@app.command()
+def private_main_to_dev():
+    """Creates a PR to merge private/main into private/dev."""
+    main_to_dev(env.git_repo_private)
 
 
 @app.command()
 def private_dev_to_main():
     """Creates a PR to merge private/dev into private/main."""
-    ctx = GitContext()
-
-    private_repo = ctx.get_private_repo()
-    private_repo_str = format_repo(private_repo)
-    private_dev = private_repo.get_branch("dev")
-
-    # create a new branch from private/dev
-    merge_branch_name = f"merge-dev-to-main-{private_dev.commit.sha[:7]}"
-    private_repo.create_git_ref(
-        ref=f"refs/heads/{merge_branch_name}",
-        sha=private_dev.commit.sha,
-    )
-
-    cp(f"Creating branch: {private_repo_str}/{merge_branch_name} from {private_repo_str}/dev @ {private_dev.commit.sha[:7]}")
-    cp(f"Creating PR: {private_repo_str}/{merge_branch_name} -> {private_repo_str}/main")
-
-    # create a PR from private/main to private/dev
-    pr = private_repo.create_pull(
-        title="Merge dev to main",
-        body="",
-        base="main",
-        head=merge_branch_name,
-    )
-
-    cp(f"✅  Created PR: [cyan link={pr.url}]{pr.title} #{pr.number}[/cyan link]")
-
+    dev_to_main(env.git_repo_private)
