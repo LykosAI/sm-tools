5f9365ff121b1055c8f3e099e0d33a2cbe7dfcf4
ionite34 <dev@ionite.io>
2024-02-15 14:00:50 -0500

    Add git_process

---
+++ src/stability_matrix_tools/utils/git_process.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+class GitProcess:
+    def __init__(self, cwd: str):
+        """Initialize a new GitRunner."""
+        self.process_path = "git"
+        self.cwd = cwd
+
+    def run_cmd(self, cmd: str, *args: str):
+        """Run a command and return the output."""
+        return subprocess.check_output([self.process_path, cmd, *args], cwd=self.cwd).decode("utf-8")
+
